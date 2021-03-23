a57219cf69591770d1e98ed4a3e1299f633754b9
damanpreet.singh <damanpreet.singh@industrybuying.com>
2021-03-23 15:53:39 +0530

    base for wattsapp ready

---
+++ src/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            {/* <h1> Sidebar Section </h1> */}
+            {/* SidebarHeader */}
+            <div className='sidebar_header'>
+                
+            </div>
+            {/* SidebarSearch */}
+            <div className='sidebar-search'>
+
+            </div>
+            {/* Sidebarchats */}
+            <div className='sidebar_chats'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
