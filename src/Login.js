90b52085b232a5649ef448eef4ef234a1b1df673
damanpreet.singh <damanpreet.singh@industrybuying.com>
2021-03-25 12:15:47 +0530

    some css changes

--- src/Login.js
+++ src/Login.js
@@ -14,7 +14,7 @@ function Login() {
         <div className='login'>
             <div className='loginContainer'>
                 <img alt='wattsapp' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png'></img>
-                <h1>Sign in to Wattsapp</h1>
+                <h2>Sign in to Wattsapp</h2>
                 <button onClick={signIn}>Sign in with Google</button>
             </div>
         </div>
