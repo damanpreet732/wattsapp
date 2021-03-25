02ba0acaf61a4a09a2cd7d2153f8020217aea7bf
damanpreet.singh <damanpreet.singh@industrybuying.com>
2021-03-25 12:13:57 +0530

    google auth added

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { auth, provider } from './Firebase'
 import './Login.css'
 
 function Login() {
+
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+            .then((result) => console.log(result))
+            .catch(error => alert(error.message))
+    }
+
     return (
         <div className='login'>
-            <h1>LOGIN</h1>
+            <div className='loginContainer'>
+                <img alt='wattsapp' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png'></img>
+                <h1>Sign in to Wattsapp</h1>
+                <button onClick={signIn}>Sign in with Google</button>
+            </div>
         </div>
     )
 }
